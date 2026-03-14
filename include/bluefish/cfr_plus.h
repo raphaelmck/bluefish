55c1e89dea0d76631e38d51d2926e58089a79693
Raphaël Michnik <michnikraphael@gmail.com>
2026-03-14 16:11:50 -0400

    Updated CFR+ header

--- include/bluefish/cfr_plus.h
+++ include/bluefish/cfr_plus.h
@@ -4,4 +4,15 @@
 
 #include "solver.h"
 
+namespace bluefish {
 
+class CfrPlusSolver final : public Solver {
+public:
+	double train(int iterations, RootFn root_fn) override;
+	std::string name() const override { return "cfr+"; }
+
+private:
+	double cfr_plus(const GameState& state, double pi0, double pi1);
+};
+
+} // namespace bluefish
